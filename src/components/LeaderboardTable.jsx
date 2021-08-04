29b2a1b048f48b3d6adef69a2c91bf3c1d82671f
Volodymyr Basok <volodymyrquo@gmail.com>
2021-08-04 15:03:16 +0300

    deleted styles directory

--- src/components/LeaderboardTable.jsx
+++ src/components/LeaderboardTable.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
-import 'common-library/syles.scss'
+import 'common-library/src/styles.scss'
 import classNames from 'classnames'
 import { people } from '../assets/helpers/people.js'
 import gold from '../assets/images/leaderboard/gold.svg'
