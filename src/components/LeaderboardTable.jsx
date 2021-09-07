bbf9fbbf868cd76471a19e7fbc595c0263f47099
Volodymyr Basok <volodymyrquo@gmail.com>
2021-09-07 12:44:56 +0300

    deleted global padding

--- src/components/LeaderboardTable.jsx
+++ src/components/LeaderboardTable.jsx
@@ -61,163 +61,161 @@ const LeaderboardTable = () => {
   }
 
   return (
-    <div style={{ padding: '30px' }}>
-      <section className='leaderboard-page'>
-        <div className='leaderboard-page__block-table'>
-          <div className='leaderboard-page__block-btn-scroll'>
-            <div className='leaderboard-page__block-btn'>
-              <button
-                className={classNames('leaderboard-page__btn', {
-                  'leaderboard-page__btn-active': activeTab === 'allTime',
-                })}
-                onClick={() => {
-                  setActiveTab('allTime')
-                }}>
-                All time
-              </button>
-              <button
-                className={classNames('leaderboard-page__btn', {
-                  'leaderboard-page__btn-active': activeTab === 'week',
-                })}
-                onClick={() => {
-                  setActiveTab('week')
-                }}>
-                This week
-              </button>
-              <button
-                className={classNames('leaderboard-page__btn', {
-                  'leaderboard-page__btn-active': activeTab === 'month',
-                })}
-                onClick={() => {
-                  setActiveTab('month')
-                }}>
-                This month
-              </button>
-              <button
-                className={classNames('leaderboard-page__btn', {
-                  'leaderboard-page__btn-active': activeTab === 'year',
-                })}
-                onClick={() => {
-                  setActiveTab('year')
-                }}>
-                This year
-              </button>
-            </div>
+    <section className='leaderboard-page'>
+      <div className='leaderboard-page__block-table'>
+        <div className='leaderboard-page__block-btn-scroll'>
+          <div className='leaderboard-page__block-btn'>
+            <button
+              className={classNames('leaderboard-page__btn', {
+                'leaderboard-page__btn-active': activeTab === 'allTime',
+              })}
+              onClick={() => {
+                setActiveTab('allTime')
+              }}>
+              All time
+            </button>
+            <button
+              className={classNames('leaderboard-page__btn', {
+                'leaderboard-page__btn-active': activeTab === 'week',
+              })}
+              onClick={() => {
+                setActiveTab('week')
+              }}>
+              This week
+            </button>
+            <button
+              className={classNames('leaderboard-page__btn', {
+                'leaderboard-page__btn-active': activeTab === 'month',
+              })}
+              onClick={() => {
+                setActiveTab('month')
+              }}>
+              This month
+            </button>
+            <button
+              className={classNames('leaderboard-page__btn', {
+                'leaderboard-page__btn-active': activeTab === 'year',
+              })}
+              onClick={() => {
+                setActiveTab('year')
+              }}>
+              This year
+            </button>
           </div>
-          <div className='leaderboard-page__block-table-fixed'>
-            <table className='leaderboard-page__block-table-fixed'>
-              <thead>
-                <tr className='leaderboard-page__table'>
-                  <th className='leaderboard-page__th'>Rank</th>
-                  <th className='leaderboard-page__th'>Username</th>
-                  <th className='leaderboard-page__th'>Invited users</th>
-                  <th className='leaderboard-page__th'>Total earnings</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentUsers.map((user, index) => (
-                  <tr key={index}>
-                    <td className='leaderboard-page__td leaderboard-page__td-width'>
-                      {index + 1}
-                      <sup className='leaderboard-page__sup'>
-                        {returnSub(index)}
-                      </sup>
-                      <img
-                        className='leaderboard-page__td-img'
-                        src={isImg(index)}
-                        alt=''
-                      />
-                    </td>
-                    <td className='leaderboard-page__td'>
-                      <img
-                        className='leaderboard-page__td-img-dolar'
-                        src={user.img}
-                        alt=''
-                      />
-                      {user.name}
-                    </td>
-                    <td className='leaderboard-page__td leaderboard-page__td-color'>
-                      <img
-                        className='leaderboard-page__th-user-icon'
-                        src={users}
-                        alt=''
-                      />
-                      {user.invited}
-                    </td>
-                    <td className='leaderboard-page__td'>
-                      {' '}
-                      <img
-                        className='leaderboard-page__td-img-dolar'
-                        src={user.dolar}
-                        alt=''
-                      />
-                      {`$ ${user.earnings.toLocaleString('en-US')}`}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-              <thead>
-                <tr className='leaderboard-page__user'>
-                  <th className='leaderboard-page__th-user leaderboard-page__th-last'>
-                    12 <sup className='leaderboard-page__sup'>th</sup>
-                  </th>
-                  <th className='leaderboard-page__th-user'>
+        </div>
+        <div className='leaderboard-page__block-table-fixed'>
+          <table className='leaderboard-page__block-table-fixed'>
+            <thead>
+              <tr className='leaderboard-page__table'>
+                <th className='leaderboard-page__th'>Rank</th>
+                <th className='leaderboard-page__th'>Username</th>
+                <th className='leaderboard-page__th'>Invited users</th>
+                <th className='leaderboard-page__th'>Total earnings</th>
+              </tr>
+            </thead>
+            <tbody>
+              {currentUsers.map((user, index) => (
+                <tr key={index}>
+                  <td className='leaderboard-page__td leaderboard-page__td-width'>
+                    {index + 1}
+                    <sup className='leaderboard-page__sup'>
+                      {returnSub(index)}
+                    </sup>
                     <img
-                      className='leaderboard-page__img-user'
-                      src={user}
+                      className='leaderboard-page__td-img'
+                      src={isImg(index)}
                       alt=''
                     />
-                    <span className='leaderboard-page__img-user-text'>
-                      Harriet Andersson
-                    </span>
-                  </th>
-                  <th className='leaderboard-page__th-user'>
-                    {' '}
+                  </td>
+                  <td className='leaderboard-page__td'>
+                    <img
+                      className='leaderboard-page__td-img-dolar'
+                      src={user.img}
+                      alt=''
+                    />
+                    {user.name}
+                  </td>
+                  <td className='leaderboard-page__td leaderboard-page__td-color'>
                     <img
                       className='leaderboard-page__th-user-icon'
                       src={users}
                       alt=''
-                    />{' '}
-                    2,575
-                  </th>
-                  <th className='leaderboard-page__th-user'>
+                    />
+                    {user.invited}
+                  </td>
+                  <td className='leaderboard-page__td'>
                     {' '}
                     <img
-                      className='leaderboard-page__th-user-img-dolar'
-                      src={dolar}
+                      className='leaderboard-page__td-img-dolar'
+                      src={user.dolar}
                       alt=''
                     />
-                    $2,135.50
-                  </th>
+                    {`$ ${user.earnings.toLocaleString('en-US')}`}
+                  </td>
                 </tr>
-              </thead>
-              <thead>
-                <tr className='leaderboard-page__table'>
-                  <th className='leaderboard-page__th leaderboard-page__th-last'>
-                    <span className='leaderboard-page__text'>
-                      {`Showing ${currentUsers.length + 1} of ${
-                        people.length + 1
-                      } operation`}
-                      <span
-                        onClick={addPeople}
-                        className='leaderboard-page__arrow'>
-                        View more
-                      </span>
+              ))}
+            </tbody>
+            <thead>
+              <tr className='leaderboard-page__user'>
+                <th className='leaderboard-page__th-user leaderboard-page__th-last'>
+                  12 <sup className='leaderboard-page__sup'>th</sup>
+                </th>
+                <th className='leaderboard-page__th-user'>
+                  <img
+                    className='leaderboard-page__img-user'
+                    src={user}
+                    alt=''
+                  />
+                  <span className='leaderboard-page__img-user-text'>
+                    Harriet Andersson
+                  </span>
+                </th>
+                <th className='leaderboard-page__th-user'>
+                  {' '}
+                  <img
+                    className='leaderboard-page__th-user-icon'
+                    src={users}
+                    alt=''
+                  />{' '}
+                  2,575
+                </th>
+                <th className='leaderboard-page__th-user'>
+                  {' '}
+                  <img
+                    className='leaderboard-page__th-user-img-dolar'
+                    src={dolar}
+                    alt=''
+                  />
+                  $2,135.50
+                </th>
+              </tr>
+            </thead>
+            <thead>
+              <tr className='leaderboard-page__table'>
+                <th className='leaderboard-page__th leaderboard-page__th-last'>
+                  <span className='leaderboard-page__text'>
+                    {`Showing ${currentUsers.length + 1} of ${
+                      people.length + 1
+                    } operation`}
+                    <span
+                      onClick={addPeople}
+                      className='leaderboard-page__arrow'>
+                      View more
                     </span>
-                  </th>
-                  <th className='leaderboard-page__th'></th>
-                  <th className='leaderboard-page__th'></th>
-                  <th className='leaderboard-page__th'></th>
-                </tr>
-              </thead>
-            </table>
-          </div>
+                  </span>
+                </th>
+                <th className='leaderboard-page__th'></th>
+                <th className='leaderboard-page__th'></th>
+                <th className='leaderboard-page__th'></th>
+              </tr>
+            </thead>
+          </table>
         </div>
-        <div className='leaderboard-page__width-share'>
-          <LoaderInformation />
-        </div>
-      </section>
-    </div>
+      </div>
+      <div className='leaderboard-page__width-share'>
+        <LoaderInformation />
+      </div>
+    </section>
   )
 }
 export default LeaderboardTable
